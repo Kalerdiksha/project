87047ab25551321f6d170567f96297f2082e3b0d
Kalerdiksha <100907581+Kalerdiksha@users.noreply.github.com>
2023-04-23 21:27:00 +0530

    Add files via upload

---
+++ Stockanalysisproject/script.js
@@ -0,0 +1,10 @@
+
+function openForm() {
+    document.getElementById("myForm-bg").style.display = "block";
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm-bg").style.display = "none";
+    document.getElementById("myForm").style.display = "none";
+}
